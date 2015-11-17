d54148147ec9b7b096f19024763f2cc03c28b65b
Philip McMahon <philip.mcmahon@guardian.co.uk>
2015-11-17 17:00:12 +0000

    a load of alerts for debugging.

--- chrome-extension/contentScript.js
+++ chrome-extension/contentScript.js
@@ -1,1 +1,6 @@
-function (getStuff)
+function getStuff() {
+    alert('getting stuff!');
+    var tableHTML = $('indexingSectionDiv_prod.academic_MSTAR_185788508');
+    console.log(tableHTML);
+    return tableHTML
+}
