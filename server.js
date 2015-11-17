393631d68349695fbd3a73d46d902f4ce5e2fc72
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2015-11-17 13:38:51 +0000

    Add initial express app

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+var exports = module.exports = {};
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+
+// get an instance of the express Router
+var router = express.Router();
+
+// The url github hits
+router.get('/pdf', function(req, res) {
+	console.log('Hello')
+	res.send('test done')
+});
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
