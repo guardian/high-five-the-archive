3100dbbc3bbd7c9b50e876cc22f4ac0187603b2c
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2015-11-17 15:37:38 +0000

    Convert pdf to image and OCR

--- server.js
+++ server.js
@@ -5,9 +5,12 @@
 var exports = module.exports = {};
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+var express    	= require('express');        // call express
+var app        	= express();                 // define our app using express
+var bodyParser 	= require('body-parser');
+var exec 		= require('child_process').exec
+var tesseract   = require('node-tesseract');
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,6 +18,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
+
+// APP FUNCTIONS
+// =============================================================================
+
+function convertToImage(filePath, opts) {
+	exec('convert -density 300 ' + filePath + ' -depth 8 tmp/' + opts.outId + '.png', function(err, stdout, stderr){
+		if (err) { 
+			console.log('It did not work');
+			return 'lose'
+		};
+
+		tesseract.process(__dirname + '/tmp/'  + opts.outId + '.png', function(err, text) {
+		    if(err) {
+		        console.error(err);
+		    } else {
+		        console.log(text);
+		    }
+		});
+	})
+}
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -24,8 +47,13 @@ var router = express.Router();
 
 // The url github hits
 router.get('/pdf', function(req, res) {
-	console.log('Hello')
-	res.send('test done')
+	// Assume we have a pdf at this point
+	var imagePath = 'testPdfs/another.pdf',
+		outId = imagePath.match('/([^/\?]+)\\.').pop();
+
+	res.send(convertToImage(imagePath, {
+		outId: outId
+	}))
 });
 // more routes for our API will happen here
 
